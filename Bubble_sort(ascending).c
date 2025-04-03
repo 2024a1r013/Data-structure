a7f8c147123a72dfc656ec9eb4aa26473901e22a
2024a1r013 <2024a1r013@mietjammu.in>
2025-04-03 10:54:28 +0530

    Create Bubble_sort(ascending).c

---
+++ Bubble_sort(ascending).c
@@ -0,0 +1,26 @@
+/sort the element of an array is ascending order using bubble sort
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter size:");
+    scanf("%d",&n);
+    int arr[n];
+    printf("enter array elements:");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    printf("After Sorting Array Element are:\n");
+    for(int i=0;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
+    return 0;
+}
